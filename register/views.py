14a791e35bb92cd3b2b11fb4a85a4bb90ea2536a
skyblue302 <bluesky595209@gmail.com>
2023-06-12 14:17:52 +0800

    v0612_1417
    favor

--- register/views.py
+++ register/views.py
@@ -28,7 +28,7 @@ def register(request):
                 #print(user[0].password)
                 temp = User(username=username,password=password)
                 temp.save()
-                favor = Favorite(username=username, stock_id='2330')
+                favor = Favorite(user_id=temp.user_id, stock_id='2330')
                 favor.save()
                 message='註冊成功 可由下方登入 !'
                 template = loader.get_template('register.html')
