d3734eb0f11cbd91f6f8333075e5dc0c13037913
skyblue302 <bluesky595209@gmail.com>
2023-06-14 14:57:02 +0800

    v0614_1456

--- home/models.py
+++ home/models.py
@@ -1,19 +1,10 @@
 from django.db import models
-
+from login.models import User
 # Create your models here.
+
 class Test(models.Model):
   num = models.CharField(max_length=255)
   cost = models.FloatField(max_length=255)
-
-class Favorite(models.Model):
-  stock_id = models.CharField(max_length=255, null=False)
-  user_id = models.CharField(max_length=255, null=False)
-  # class Meta:
-  #       constraints = [
-  #           models.UniqueConstraint(
-  #               fields=['stock_id', 'user_id'], name='unique_migration_host_combination'
-  #           )
-  #       ]
 
 class stock_info(models.Model):
   stock_id = models.CharField(max_length=255, primary_key=True)
@@ -21,3 +12,14 @@ class stock_info(models.Model):
   industry = models.CharField(max_length=255)
   address = models.CharField(max_length=255, default="")
   telephone = models.CharField(max_length=255, default="")
+
+class Favorite(models.Model):
+  stock_id = models.ForeignKey(stock_info, on_delete=models.CASCADE)
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  # class Meta:
+  #       constraints = [
+  #           models.UniqueConstraint(
+  #               fields=['stock_id', 'user_id'], name='unique_migration_host_combination'
+  #           )
+  #       ]
+
